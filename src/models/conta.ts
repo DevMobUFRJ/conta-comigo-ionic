4718b3e459aaf1c1cf2de2a9a3c3831b2ab668be
silviomm <silviommattos@gmail.com>
2018-09-23 11:25:41 -0300

    modelagem do banco do aplicativo, serviço e exemplo de como fazer

--- src/models/conta.ts
+++ src/models/conta.ts
@@ -1,4 +1,12 @@
+import { Consumidor } from "./consumidor";
+import { Pessoa } from "./pessoa";
+import { Produto } from "./produto";
+
 export interface Conta {
-    nome: string,
+    id: number
+    nome: string
+    pessoas: Array<Pessoa>
+    produtos: Array<Produto>
+    consumidores: Array<Consumidor>
     total: number
 }
