a3404292a3b1c2efc605ed6a3b310d1069b8fb99
Silvio <silviommattos@gmail.com>
2019-02-11 22:37:21 +0000

    Divisão de produto por pessoa

--- src/services/produto-service.ts
+++ src/services/produto-service.ts
@@ -3,6 +3,7 @@ import { ContaService } from "./conta-service";
 import { LoadingController } from "ionic-angular";
 import { Storage } from '@ionic/storage';
 import { Produto } from "../models/produto";
+import { Conta } from "../models/conta";
 
 
 @Injectable()
@@ -13,22 +14,24 @@ export class ProdutoService {
     private _loadingCtrl: LoadingController) {
   }
 
-  public add(produto: Produto): void {
+  public add(produto: Produto, conta: Conta): void {
     const load = this._loadingCtrl.create({ content: 'Salvando produto...' });
     load.present();
-    this._contaService.getConta().then(c => {
-      this.storage.get('produtos').then(ps => {
-        if (ps == undefined || ps == null) {
-          this.storage.set('produtos', [produto]);
-        }
-        else {
-          this.storage.set('produtos', ps.concat([produto]))
-        }
-      });
-      c.produtos.push(produto);
-      this._contaService.updateConta(c);
+    this.storage.get('produtos').then(ps => {
+      this.addApenasProduto(ps, produto);
+      conta.produtos.push(produto);
+      this._contaService.updateConta(conta);
       load.dismiss();
     });
+  }
+
+  private addApenasProduto(ps: Array<Produto>, produto: Produto): void {
+    if (ps == undefined || ps == null) {
+      this.storage.set('produtos', [produto]);
+    }
+    else {
+      this.storage.set('produtos', ps.concat([produto]))
+    }
   }
 
 }
