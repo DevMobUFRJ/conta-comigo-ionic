2b6ecf870810942549f6c53a504e0361deee5fcb
Silvio <silviommattos@gmail.com>
2019-02-11 22:37:21 +0000

    Divisão de produto por pessoa

--- src/pages/produto-modal/produto-modal.ts
+++ src/pages/produto-modal/produto-modal.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, LoadingController } from 'ionic-angular';
 import { Produto } from '../../models/produto';
 import { ProdutoService } from '../../services/produto-service';
+import { ContaService } from '../../services/conta-service';
+import { Conta } from '../../models/conta';
+import { Consumidor } from '../../models/consumidor';
 
 @IonicPage()
 @Component({
@@ -11,20 +14,54 @@ import { ProdutoService } from '../../services/produto-service';
 export class ProdutoModalPage {
 
   public produto: Produto = <Produto>{};
+  public conta: Conta = <Conta>{};
+
+  public quantidadeConsumida: number;
+  public qntdsDiferentes: boolean = false;
+  public marcadas: Array<boolean> = [];
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private _produtoService: ProdutoService,
+    private _contaService: ContaService,
+    private _loadingCtrl: LoadingController,
     public _viewCtrl: ViewController) {
+  }
+
+  ionViewWillEnter() {
+    let loadingConta = this._loadingCtrl.create({ content: 'Carregando...' });
+    loadingConta.present();
+    this._contaService.getConta().then(c => {
+      this.conta = c;
+      this.conta.pessoas.forEach(p => this.marcadas.push(false));
+      loadingConta.dismiss();
+    });
   }
 
   ionViewDidLoad() {
   }
 
   public add(): void {
-    this._produtoService.add(this.produto);
+    let cont = 0;
+    this.marcadas.forEach((m, i) => {
+      if (m) {
+        cont++;
+        this.conta.consumidores.push(<Consumidor>{
+          pessoa: this.conta.pessoas[i],
+          produto: this.produto,
+          quantidade: 0
+        });
+      }
+    });
+    const qntdIndividual = this.quantidadeConsumida / cont;
+    this.conta.consumidores.forEach(c => c.quantidade = qntdIndividual);
+    this._produtoService.add(this.produto, this.conta);
     this.dismiss();
   }
+
+  // public mostraQntdDiferente(i: number) {
+  //   return this.qntdsDiferentes && this.marcadas[i];
+  // }
 
   dismiss() {
     this._viewCtrl.dismiss({ 'produto': this.produto });
