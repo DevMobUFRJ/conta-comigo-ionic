8f464d8f0e9f9f132644b35cd0928ae4d8d2e869
Silvio <silviommattos@gmail.com>
2019-02-14 23:20:08 +0000

    lógica e front para gorjeta

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, Events } from 'ionic-angular';
 import { Conta } from '../../models/conta';
 import { ProdutoModalPage } from '../produto-modal/produto-modal';
+import { Consumidor } from '../../models/consumidor';
 
 @IonicPage()
 @Component({
@@ -13,15 +14,20 @@ import { ProdutoModalPage } from '../produto-modal/produto-modal';
 export class ProdutosPage {
 
   public conta: Conta;
-  public produtosPessoas: Array<ProdutoPessoa>
+  public produtosPessoas: Array<ProdutoPessoa>;
+
+  public valorGorjeta: number = 10;
+  public gorjetaIs: boolean = false;
 
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public modalCtrl: ModalController,
-    private _contaService: ContaService,
-    public _events: Events) {
+    public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, private _contaService: ContaService, public _events: Events) {
     this.conta = this.navParams.data;
+
+    this._events.subscribe('update-gorjeta', (valor, is) => {
+      this.valorGorjeta = valor;
+      this.gorjetaIs = is;
+    });
+    
     this._events.subscribe('update-conta', (conta) => {
       this.conta = conta
       this.initExpandableList();
@@ -51,6 +57,30 @@ export class ProdutosPage {
       }
     });
     modal.present();
+  }
+
+  public calcValorTotal(produtoPessoa: ProdutoPessoa): string {
+    return `R$ ${(produtoPessoa.valorTotal * (this.gorjetaIs ? 1 + (this.valorGorjeta / 100) : 1)).toFixed(2)}`
+  }
+
+  public calcValorPorProduto(consumidor: Consumidor): string {
+    return `${consumidor.pessoa.nome} - ${consumidor.quantidade} x ${(consumidor.produto.preco * (this.gorjetaIs ? 1 + (this.valorGorjeta / 100) : 1)).toFixed(2)} =
+    R$ ${(consumidor.produto.preco * consumidor.quantidade * (this.gorjetaIs ? 1 + (this.valorGorjeta / 100) : 1)).toFixed(2)}`;
+  }
+
+  public maisGorjeta() {
+    this.valorGorjeta++;
+    this._events.publish('update-gorjeta', this.valorGorjeta, this.gorjetaIs);
+  }
+
+  public switchGorjeta() {
+    this.gorjetaIs = !this.gorjetaIs;
+    this._events.publish('update-gorjeta', this.valorGorjeta, this.gorjetaIs);
+  }
+
+  public menosGorjeta() {
+    this.valorGorjeta--;
+    this._events.publish('update-gorjeta', this.valorGorjeta, this.gorjetaIs);
   }
 
 }
